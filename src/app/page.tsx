4518f70260b0c55b145c311bbd0ab7371ca2620c
NaeemHk011 <itxkhaskheli@gmail.com>
2024-05-12 15:27:56 +0500

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Hero from "./components/Hero";
 import ProductData from "./components/ProductData";
 import Testimonial from "./components/Testimonial";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
